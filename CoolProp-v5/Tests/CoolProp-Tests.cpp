f716761b7907b79bea556f3a65dc994ed19a4563
Ian bell <ian.h.bell@gmail.com>
2014-03-05 20:11:29 +0100

    Added structure of tests for state class init
    Signed-off-by: Ian bell <ian.h.bell@gmail.com>

--- CoolProp-v5/Tests/CoolProp-Tests.cpp
+++ CoolProp-v5/Tests/CoolProp-Tests.cpp
@@ -5,11 +5,38 @@
 // ############################################
 
 #if defined(ENABLE_CATCH)
-#include "Catch/catch.hpp"
+
+#include "../Catch/catch.hpp"
 
 TEST_CASE("State Class Instantiation","")
 {
-
+	SECTION("Bad Fluid")
+	{
+	}
+	SECTION("Pure Fluid")
+	{
+	}
+	SECTION("Pseudo-Pure Fluid")
+	{
+	}
+	SECTION("Mixture")
+	{
+	}
+	SECTION("Brine from Melinder")
+	{
+	}
+	SECTION("Incompressible Liquid")
+	{
+	}
+	SECTION("REFPROP Pure Fluid")
+	{
+	}
+	SECTION("REFPROP Pseudo-Pure Fluid")
+	{
+	}
+	SECTION("REFPROP Mixture")
+	{
+	}
 }
 
 #endif
