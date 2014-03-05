aa3c9eadcaa4d425a91f99f34c7ec2e64626e8d8
jowr <jowr@mek.dtu.dk>
2014-03-05 19:49:19 +0100

    Added cpp

--- CoolProp-v5/CoolProp.cpp
+++ CoolProp-v5/CoolProp.cpp
@@ -1,11 +1,45 @@
 
+#include "CoolProp.h"
 
-// ############################################
-//                      TESTS
-// ############################################
+static int HashableObject::getHash(){
+	return 0;
+}
 
-#if defined(ENABLE_CATCH)
-#include "Catch/catch.hpp"
+StateObject::StateObject
+{
+	public:
+	StateObject() {
+	    	// First we need to fill names and concentrations to
+	    	// provide input to the hashing function!
+	    	CoolPropClass::stateList.insert(std::make_pair<int, StateObject* >(this->getHash(),this));
+	    }
+
+		~StateObject(){
+			CoolPropClass::stateList.erase(this->getHash());
+		}
+	};
+
+class FluidObject : public HashableObject
+	{
+	public:
+		FluidObject() {
+	    	// First we need to fill names and concentrations to
+	    	// provide input to the hashing function!
+	    	CoolPropClass::fluidList.insert(std::make_pair<int, FluidObject* >(this->getHash(),this));
+		}
+
+		~FluidObject(){
+			CoolPropClass::fluidList.erase(this->getHash());
+		}
+	};
 
 
-#endif
+class CoolPropClass {
+
+  public:
+	static std::map<int, StateObject* > stateList;
+	static std::map<int, FluidObject* > fluidList;
+
+
+
+};
--- CoolProp-v5/CoolProp.h
+++ CoolProp-v5/CoolProp.h
@@ -6,37 +6,21 @@
 #include <map>
 
 class HashableObject {
-	protected:
-	    int getHash(){return 0; }
-	};
+	static int getHash();
+};
 
-class StateObject : public HashableObject
-	{
-	public:
-	StateObject() {
-	    	// First we need to fill names and concentrations to
-	    	// provide input to the hashing function!
-	    	CoolPropClass::stateList.insert(std::make_pair<int, StateObject* >(this->getHash(),this));
-	    }
-
-		~StateObject(){
-			CoolPropClass::stateList.erase(this->getHash());
-		}
-	};
+class StateObject : public HashableObject{
+public:
+	StateObject();
+	~StateObject();
+};
 
 class FluidObject : public HashableObject
-	{
-	public:
-		FluidObject() {
-	    	// First we need to fill names and concentrations to
-	    	// provide input to the hashing function!
-	    	CoolPropClass::fluidList.insert(std::make_pair<int, FluidObject* >(this->getHash(),this));
-		}
-
-		~FluidObject(){
-			CoolPropClass::fluidList.erase(this->getHash());
-		}
-	};
+{
+public:
+	FluidObject();
+	~FluidObject();
+};
 
 
 class CoolPropClass {
