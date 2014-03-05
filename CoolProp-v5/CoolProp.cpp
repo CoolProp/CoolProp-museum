3ab3f47d792224cec84230d708bf6e7b85cadb7f
Ian bell <ian.h.bell@gmail.com>
2014-03-05 20:11:42 +0100

    Added first hashing function for string
    Signed-off-by: Ian bell <ian.h.bell@gmail.com>

--- CoolProp-v5/CoolProp.cpp
+++ CoolProp-v5/CoolProp.cpp
@@ -1,45 +1,39 @@
 
 #include "CoolProp.h"
 
-static int HashableObject::getHash(){
-	return 0;
+static long HashableObject::getHash(std::string hash_string){
+	int summer = 0;
+	for (unsigned int i = 0; i < hash_string.length(); i++)
+	{
+		summer += (int)hash_string[i];
+	}
+	return summer;
 }
 
-StateObject::StateObject
-{
-	public:
-	StateObject() {
-	    	// First we need to fill names and concentrations to
-	    	// provide input to the hashing function!
-	    	CoolPropClass::stateList.insert(std::make_pair<int, StateObject* >(this->getHash(),this));
-	    }
+StateObject::StateObject() {
+	// First we need to fill names and concentrations to
+	// provide input to the hashing function!
+	CoolPropClass::stateList.insert(std::make_pair<int, StateObject* >(this->getHash(),this));
+}
 
-		~StateObject(){
-			CoolPropClass::stateList.erase(this->getHash());
-		}
-	};
+StateObject::~StateObject(){
+	CoolPropClass::stateList.erase(this->getHash());
+}
 
-class FluidObject : public HashableObject
-	{
-	public:
-		FluidObject() {
-	    	// First we need to fill names and concentrations to
-	    	// provide input to the hashing function!
-	    	CoolPropClass::fluidList.insert(std::make_pair<int, FluidObject* >(this->getHash(),this));
-		}
+FluidObject::FluidObject() {
+	// First we need to fill names and concentrations to
+	// provide input to the hashing function!
+	CoolPropClass::fluidList.insert(std::make_pair<int, FluidObject* >(this->getHash(),this));
+}
 
-		~FluidObject(){
-			CoolPropClass::fluidList.erase(this->getHash());
-		}
-	};
-
+FluidObject::~FluidObject(){
+	CoolPropClass::fluidList.erase(this->getHash());
+}
 
 class CoolPropClass {
 
   public:
-	static std::map<int, StateObject* > stateList;
-	static std::map<int, FluidObject* > fluidList;
-
-
+	static std::map<int, StateObject*> stateList;
+	static std::map<int, FluidObject*> fluidList;
 
 };
